5e7a1d6637fd81112e7686955d74aa7d3dfe7f46
Grant Chen <grantchen2003@gmail.com>
2026-04-10 04:10:26 -0400

    feat: initialize gRPC service and protobuf definitions
    - Add gRPC dependencies and protobuf-maven-plugin to pom.xml
    - Define TxManagerRedisService and CommitTransaction messages in tx_manager_redis.proto
    - Add Main entry point to start the gRPC server on port 9090
    - Implement TxManagerRedisServiceImpl with one-shot transaction logic and sequence verification stub

---
+++ src/main/java/io/github/grantchen2003/cdb/tx/manager/redis/TxManagerRedisServiceImpl.java
@@ -0,0 +1,34 @@
+package io.github.grantchen2003.cdb.tx.manager.redis;
+
+import io.github.grantchen2003.cdb.tx.manager.redis.grpc.CommitTransactionRequest;
+import io.github.grantchen2003.cdb.tx.manager.redis.grpc.CommitTransactionResponse;
+import io.github.grantchen2003.cdb.tx.manager.redis.grpc.Operation;
+import io.github.grantchen2003.cdb.tx.manager.redis.grpc.TxManagerRedisServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+import java.util.List;
+
+public class TxManagerRedisServiceImpl extends TxManagerRedisServiceGrpc.TxManagerRedisServiceImplBase {
+
+    @Override
+    public void commitTransaction(CommitTransactionRequest request, StreamObserver<CommitTransactionResponse> responseObserver) {
+        final long expectedSeqNum = request.getExpectedSeqNum();
+        final List<Operation> operations = request.getOperationsList();
+
+        System.out.println(expectedSeqNum);
+
+        for (final Operation op : operations) {
+            System.out.println(op.getOpType());
+            System.out.println(op.getTable());
+            System.out.println(op.getData());
+        }
+
+        final CommitTransactionResponse response = CommitTransactionResponse.newBuilder()
+                .setStatus(CommitTransactionResponse.Code.SUCCESS)
+                .setAppliedSeqNum(expectedSeqNum + 1)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
